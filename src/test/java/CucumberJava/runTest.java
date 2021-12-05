13fdfcd7f71225aa515c354c3532923f819dbebe
balajitpgit <69788055+balajitpgit@users.noreply.github.com>
2021-12-05 19:56:25 +0530

    Automated mail compose scenario

--- src/test/java/CucumberJava/runTest.java
+++ src/test/java/CucumberJava/runTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber; 
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber"}) 
+@Cucumber.Options(format = {"pretty", "html:target"}) 
 
 public class runTest { }
 
